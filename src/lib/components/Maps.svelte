e6e98f16733f0a7ee06780eefcad1eaff81bd45b
has1234513 <hashaydar29@gmail.com>
2023-07-18 22:32:20 +0200

    solved api key

--- src/lib/components/Maps.svelte
+++ src/lib/components/Maps.svelte
@@ -1,30 +1,60 @@
 <script>
 // @ts-nocheck
 
-  import mapStyles from './map-styles'; // optional
+  import { onMount } from 'svelte';
+  let map;
+  let marker;
+  let mapElement;
 
+  // Replace with your own Google Maps API key
+  const apiKey = 'My_Api_key';
 
- let container;
- let map;
- let zoom = 8;
-   let center = {lat: -34.397, lng: 150.644};
-   
-   import { onMount } from 'svelte';
-   
- onMount(async () => {
-   map = new google.maps.Map(container, {
-           zoom,
-     center,
-     styles: mapStyles // optional
-   });
- });
+  // Replace with the initial coordinates of the GPS device
+  const initialPosition = { lat: -34.397, lng: 150.644 };
+
+  onMount(async () => {
+    // Load the Google Maps JavaScript API script
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Wait for the Google Maps JavaScript API script to load
+    await new Promise(resolve => {
+      script.onload = resolve;
+    });
+
+    // Create the map
+    map = new google.maps.Map(mapElement, {
+      center: initialPosition,
+      zoom: 8,
+    });
+
+    // Create the marker
+    marker = new google.maps.Marker({
+      position: initialPosition,
+      map,
+    });
+
+    // Update the marker position in real-time as new GPS data becomes available
+    // Replace this with your own code to get the GPS data
+    setInterval(() => {
+      const newPosition = getNewPositionFromGPS();
+      marker.setPosition(newPosition);
+      map.panTo(newPosition);
+    }, 1000);
+  });
 </script>
 
-<style>
-.full-screen {
-   width: 100vw;
-   height: 100vh;
-}
-</style>
+<svelte:head>
+  <style>
+    #map {
+      height: 400px;
+      width: 600px
+    }
+  </style>
+</svelte:head>
 
-<div class="full-screen" bind:this={container}></div>
+<h1>Maps is supposed to be here</h1>
+
+<div id="map" bind:this={mapElement}></div>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -2,21 +2,14 @@
 // @ts-nocheck
 
 	import Map from '../../lib/components/Maps.svelte';
-	export let ready;
-  let GOOGLE_MAPS_API_KEY = "fsdfsdf"
 </script>
 
-<svelte:head>
-	<script defer async
-	src="https://maps.googleapis.com/maps/api/js?key={GOOGLE_MAPS_API_KEY}&callback=initMap">
-	</script>
-</svelte:head>
 
 <div>
   <h1>Map</h1>
 
-  { #if ready }
+
   <Map/>
-  { /if }
-  </div>
+
+</div>
 
